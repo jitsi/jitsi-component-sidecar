b57f97c398ffe939da1b3ebb1e365af337f0d1e9
Дамян Минков <damencho@jitsi.org>
2022-04-26 09:28:50 -0500

    squash: Updates service URLs defaults based on the component type.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv';
 import envalid from 'envalid';
 
+import { ComponentType } from '../service/stats_reporter';
+
 const result = dotenv.config();
 
 if (result.error) {
@@ -29,9 +31,9 @@ const env = envalid.cleanEnv(process.env, {
     REQUEST_RETRY_COUNT: envalid.num({ default: 2 }),
     STATS_POLLING_INTERVAL: envalid.num({ default: 30 }),
     STATS_REPORTING_INTERVAL: envalid.num({ default: 30 }),
-    STATS_RETRIEVE_URL: envalid.str(),
-    START_INSTANCE_URL: envalid.str(),
-    STOP_INSTANCE_URL: envalid.str(),
+    STATS_RETRIEVE_URL: envalid.str({ default: undefined }),
+    START_INSTANCE_URL: envalid.str({ default: undefined }),
+    STOP_INSTANCE_URL: envalid.str({ default: undefined }),
     ENABLE_STOP_INSTANCE: envalid.bool({ default: true }),
     ENVIRONMENT: envalid.str({ default: 'default-env' }),
     REGION: envalid.str({ default: 'default-region' }),
@@ -63,9 +65,12 @@ export default {
 
     // number of seconds to wait before reporting stats
     StatsReportingInterval: env.STATS_REPORTING_INTERVAL,
-    StatsRetrieveURL: env.STATS_RETRIEVE_URL,
-    StartComponentURL: env.START_INSTANCE_URL,
-    StopComponentURL: env.STOP_INSTANCE_URL,
+    StatsRetrieveURL: env.STATS_RETRIEVE_URL || (env.COMPONENT_TYPE === ComponentType.Jigasi
+        ? 'http://localhost:8788/about/health' : 'http://localhost:2222/jibri/api/v1.0/health'),
+    StartComponentURL: env.START_INSTANCE_URL || (env.COMPONENT_TYPE === ComponentType.Jigasi
+        ? 'http://localhost:8788/api/v1.0/startService' : 'http://localhost:2222/jibri/api/v1.0/startService'),
+    StopComponentURL: env.STOP_INSTANCE_URL || (env.COMPONENT_TYPE === ComponentType.Jigasi
+        ? 'http://localhost:8788/api/v1.0/stopService' : 'http://localhost:2222/jibri/api/v1.0/stopService'),
     EnableStopComponent: env.ENABLE_STOP_INSTANCE,
     Environment: env.ENVIRONMENT,
     Region: env.REGION,
